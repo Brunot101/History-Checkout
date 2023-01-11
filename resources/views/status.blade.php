a80fb8b46c1618fb91fd4bb0a181c3b7021ca283
Brunot101 <contato.brunoluiz101@gmail.com>
2023-01-11 16:45:56 -0300

    Working on progress bar

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -6,13 +6,34 @@
                 <div class="head" >
                     <h2>Status do Histórico</h2>
                 </div>
+                <div class="cline"></div>
                 <ul class="list-unstyled etapas">
-                    <li class="solicitado" {{$historico->status == 'Solicitado' ? 'style=background-color:rgb(8,224,8);' : ''}} >Solicitado</li>
-                    <li class="pasta-encontrada" {{$historico->status == 'Pasta encontrada' ? 'style=background-color:rgb(8,224,8);' : ''}}>Pasta encontrada</li>
-                    <li class="pendencias-verificadas" {{$historico->status == 'Pendencias verificadas' ? 'style=background-color:rgb(8,224,8);' : ''}}>Pendecias verificadas</li>
-                    <li class="historico-em-confecção" {{$historico->status == 'Em confeccao' ? 'style=background-color:rgb(8,224,8);' : ''}}>Histórico em confecção</li>
-                    <li class="historico-pronto" {{$historico->status == 'Historico pronto' ? 'style=background-color:rgb(8,224,8);' : ''}}>Histórico pronto</li>
-                    <p></p>
+                    <li class="solicitado" data-toggle="tab" >
+                        <span class="round-tab"  {{$historico->status == 'Solicitado' ? 'style=background-color:rgb(8,224,8);' : ''}}>
+                            <i class="bi bi-file-earmark"></i>
+                        </span>
+                    </li>
+                    <li class="pasta-encontrada" data-toggle="tab" >
+                        <span class="round-tab" {{$historico->status == 'Pasta encontrada' ? 'style=background-color:rgb(8,224,8);' : ''}}>
+                            <i class="bi bi-file-earmark-check"></i>
+                        </span>
+                    </li>
+                    <li class="pendencias-verificadas" data-toggle="tab" >
+                        <span class="round-tab" {{$historico->status == 'Pendencias verificadas' ? 'style=background-color:rgb(8,224,8);' : ''}}>
+                            <i class="bi bi-search"></i>
+                        </span>
+                    </li>
+                    <li class="historico-em-confecção" data-toggle="tab" >
+                        <span class="round-tab" {{$historico->status == 'Em confeccao' ? 'style=background-color:rgb(8,224,8);' : ''}}>
+                            <i class="bi bi-brush" ></i>
+                        </span>
+                    </li>
+                    <li class="historico-pronto" data-toggle="tab"  >
+                        <span class="round-tab" {{$historico->status == 'Historico pronto' ? 'style=background-color:rgb(8,224,8);' : ''}}>
+                            <i class="bi bi-check2"></i>
+                        </span>
+                    </li>
+                    
                 </ul>
             </div>
         @endsection
